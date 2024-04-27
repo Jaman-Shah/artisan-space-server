91f799c1d9c58d2f42e9d8e43c13f68f6246f6c5
Jaman-Shah <jamanshah5400@gmail.com>
2024-04-27 21:06:53 +0600

    delete route created successfully

--- index.js
+++ index.js
@@ -69,6 +69,15 @@ async function run() {
       res.send(result);
     });
 
+    // deleting craft by id
+
+    app.delete("/crafts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await allAddedCraftCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
